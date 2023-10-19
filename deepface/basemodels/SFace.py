17f8c152b84f787d3e1e38e0fcb2f62811b4b92f
dakyuz92 <46398917+dakyuz92@users.noreply.github.com>
2023-10-18 22:53:37 -0700

    Add files via upload

---
+++ deepface/basemodels/SFace.py
@@ -0,0 +1,47 @@
+import os
+import zipfile
+import gdown
+import tensorflow as tf
+from deepface.commons import functions
+
+# --------------------------------
+# dependency configuration
+
+tf_version = int(tf.__version__.split(".", maxsplit=1)[0])
+
+if tf_version == 1:
+    from keras.models import Model, Sequential
+    from keras.layers import (
+        Convolution2D,
+        LocallyConnected2D,
+        MaxPooling2D,
+        Flatten,
+        Dense,
+        Dropout,
+    )
+else:
+    from tensorflow.keras.models import Model, Sequential
+    from tensorflow.keras.layers import (
+        Convolution2D,
+        LocallyConnected2D,
+        MaxPooling2D,
+        Flatten,
+        Dense,
+        Dropout,
+    )
+
+
+# -------------------------------------
+# pylint: disable=line-too-long
+
+
+def loadModel(
+    url="https://github.com/swghosh/DeepFace/releases/download/weights-vggface2-2d-aligned/VGGFace2_DeepFace_weights_val-0.9034.h5.zip",
+):
+    base_model = Sequential()
+    base_model.add(
+        Convolution2D(32, (11, 11), activation="relu", name="C1", input_shape=(152, 152, 3))
+    )
+    base_model.add(MaxPooling2D(pool_size=3, strides=2, padding="same", name="M2"))
+    base_model.add(Convolution2D(16, (9, 9), activation="relu", name="C3"))
+    base_model.add(
